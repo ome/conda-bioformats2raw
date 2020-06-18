07f9c132b04b0d45bc5e8c42544ae714d700a039
jmoore <josh@glencoesoftware.com>
2020-06-18 15:33:42 +0200

    Include blosc for test

--- bld.bat
+++ bld.bat
@@ -1,4 +1,5 @@
 mkdir %LIBRARY_PREFIX%\bin
-copy %SRC_DIR%\*.bat %LIBRARY_PREFIX%\bin
-copy %SRC_DIR%\*.jar %LIBRARY_PREFIX%\bin
+mkdir %LIBRARY_PREFIX%\lib
+copy %SRC_DIR%\bin\*.bat %LIBRARY_PREFIX%\bin
+copy %SRC_DIR%\lib\*.jar %LIBRARY_PREFIX%\lib
 if errorlevel 1 exit 1
