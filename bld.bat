31c93c84762ac5b0e1bd079efc76df5059c58286
Simon Li <spli@dundee.ac.uk>
2020-10-27 20:15:49 +0000

    windows PREFIX instead of LIBRARY_PREFIX

--- bld.bat
+++ bld.bat
@@ -1,8 +1,9 @@
 set pkg=%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
 
-mkdir %LIBRARY_PREFIX%\bin
-mkdir %LIBRARY_PREFIX%\share\%pkg%
-xcopy %SRC_DIR%\* %LIBRARY_PREFIX%\share\%pkg%\ /E
+mkdir %PREFIX%\bin
+mkdir %PREFIX%\share\%pkg%
+xcopy %SRC_DIR%\* %PREFIX%\share\%pkg%\ /E
+if errorlevel 1 exit 1
 
-echo %%~dp0\..\share\%pkg%\bin\bioformats2raw %%* > %LIBRARY_PREFIX%\bin\bioformats2raw.bat
+echo %%~dp0..\share\%pkg%\bin\bioformats2raw %%* > %PREFIX%\bin\bioformats2raw.bat
 if errorlevel 1 exit 1
