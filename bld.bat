9d050190876e8e04a6370f65855425019f4c2021
Simon Li <spli@dundee.ac.uk>
2020-10-27 19:15:12 +0000

    Attempt to use share/PKG_NAME-PKG_VERSION-PKG_BUILDNUM

--- bld.bat
+++ bld.bat
@@ -1,5 +1,8 @@
+set pkg=%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
+
 mkdir %LIBRARY_PREFIX%\bin
-mkdir %LIBRARY_PREFIX%\lib
-copy %SRC_DIR%\bin\*.bat %LIBRARY_PREFIX%\bin
-copy %SRC_DIR%\lib\*.jar %LIBRARY_PREFIX%\lib
+mkdir %LIBRARY_PREFIX%\share\%pkg%
+xcopy %SRC_DIR%\* %LIBRARY_PREFIX%\share\%pkg%\ /E
+
+echo %%~dp0\..\share\%pkg%\bin\bioformats2raw %%* > %LIBRARY_PREFIX%\bin\bioformats2raw.bat
 if errorlevel 1 exit 1
--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 set -eu -o pipefail
 
 sharedir=$PREFIX/share
-outdir=$sharedir/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
-mkdir -p $PREFIX/bin
+pkg=$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+outdir=$sharedir/$pkg
+mkdir -p $outdir $PREFIX/bin
 
-cp -R $SRC_DIR/* $sharedir
-# Provide a link to the scripts from $PREFIX/bin.
-find $sharedir -type f -perm -111 -exec ln -s {} $PREFIX/bin \;
+cp -R $SRC_DIR/* $outdir
+ln -s ../share/$pkg/bin/bioformats2raw $PREFIX/bin/bioformats2raw
