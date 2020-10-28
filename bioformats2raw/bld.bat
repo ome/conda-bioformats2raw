38c4d56aec5399836807dc5263a575c9607d17ee
Simon Li <spli@dundee.ac.uk>
2020-10-28 14:14:21 +0000

    Split jars into separate package
    This means we have a noarch jars package, and arch-specific wrapper packages. Since the jars package is very large this reduces the space used on our Anaconda cloud account.

---
+++ bioformats2raw/bld.bat
@@ -0,0 +1,9 @@
+set pkg=bioformats2raw-%PKG_VERSION%-%PKG_BUILDNUM%
+
+mkdir %PREFIX%\share\%pkg%\bin
+copy %SRC_DIR%\bin\bioformats2raw.bat %PREFIX%\share\%pkg%\bin\
+if errorlevel 1 exit 1
+
+mkdir %PREFIX%\bin
+echo %%~dp0..\share\%pkg%\bin\bioformats2raw %%* > %PREFIX%\bin\bioformats2raw.bat
+if errorlevel 1 exit 1
--- bld.bat
+++
@@ -1,9 +0,0 @@
-set pkg=%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
-
-mkdir %PREFIX%\bin
-mkdir %PREFIX%\share\%pkg%
-xcopy %SRC_DIR%\* %PREFIX%\share\%pkg%\ /E
-if errorlevel 1 exit 1
-
-echo %%~dp0..\share\%pkg%\bin\bioformats2raw %%* > %PREFIX%\bin\bioformats2raw.bat
-if errorlevel 1 exit 1
